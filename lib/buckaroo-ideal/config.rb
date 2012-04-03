ac1be7386d2b3121022d34cdbe5ac5f02755cef9
Tom-Eric Gerritsen <ik@tom-eric.info>
2012-04-03 13:25:46 +0200

    Add more settings to Buckaroo::Ideal::Config and make it's defaults come from a method instead of a constant

--- lib/buckaroo-ideal/config.rb
+++ lib/buckaroo-ideal/config.rb
@@ -5,26 +5,75 @@ module Buckaroo
   module Ideal
     class Config
       class << self
-        DEFAULTS = {
-          merchant_key: nil,
-          secret_key:   nil,
-          test_mode:    false
-        }
-        
-        # @return [String] The merchant-key supllied by Buckaroo
+        # The merchant-key is supllied by Buckaroo. Every application MUST have
+        # it's own merchant key.
+        #
+        # @return [String] The merchant-key for the application
         attr_accessor :merchant_key
         
+        # The secret_key should only be known by your application and Buckaroo.
+        # It is used to sign orders and validate transactions.
+        #
         # @return [String] The shared secret key that is used to sign
         #   transaction requests
         attr_accessor :secret_key
         
-        # @return [Boolean] Enable/disables test mode. If set to +true+, no
-        #   money is actually transferred by completing a transaction
+        # If test_mode is enabled, transactions will be registered by Buckaroo,
+        # but clients will not be forwared to the iDEAL page of their bank.
+        #
+        # Clients will be redirected back to the success_url of your application
+        #
+        # @return [Boolean] Test mode on/off
         attr_accessor :test_mode
+        
+        # @return [String] The URL the user will be redirected to after a
+        #   successful transaction
+        attr_accessor :success_url
+        
+        # @return [String] The URL the user will be redirected to after a failed
+        #   transaction
+        attr_accessor :reject_url
+        
+        # @return [String] The URL the user will be redirected to after an error
+        #   occured during the transaction
+        attr_accessor :error_url
+        
+        # @return [String] The HTTP method that will be used to return the user
+        #   back to the application after a transaction
+        attr_accessor :return_method
+        
+        # There are 2 styles that you can use to integrate Buckaroo iDEAL:
+        #  * POPUP - The transaction is performed in a popup
+        #  * PAGE  - The transaction is performed in the original window
+        #
+        # @return [String] The style that is being used
+        attr_accessor :style
+        
+        # If the POPUP style is being used, you can autoclose the popup after
+        # a transaction. You will have to provide information about the
+        # transaction to the user on the page he will arrive on.
+        #
+        # @return [Boolean] Autoclose the popup after a transaction
+        attr_accessor :autoclose_popup
+        
+        # Default settings
+        def defaults
+          {
+            merchant_key:    nil,
+            secret_key:      nil,
+            test_mode:       false,
+            success_url:     nil,
+            reject_url:      nil,
+            error_url:       nil,
+            return_method:   'POST',
+            style:           'PAGE',
+            autoclose_popup: false
+          }
+        end
         
         # Configure the integration with Buckaroo
         def configure(settings = {})
-          DEFAULTS.merge(settings).each do |key, value|
+          defaults.merge(settings).each do |key, value|
             set key, value
           end
         end
--- spec/buckaroo-ideal/config_spec.rb
+++ spec/buckaroo-ideal/config_spec.rb
@@ -3,28 +3,66 @@ require 'spec_helper'
 describe Buckaroo::Ideal::Config do
   before do
     Buckaroo::Ideal::Config.configure(
-      merchant_key: 'merchant_key',
-      secret_key:   'secret_key',
-      test_mode:    true
+      merchant_key:   'merchant_key',
+      secret_key:     'secret_key',
+      test_mode:      true,
+      success_url:    'http://example.com/transaction/success',
+      reject_url:     'http://example.com/transaction/reject',
+      error_url:      'http://example.com/transaction/error',
+      return_method:  'GET',
+      style:          'POPUP',
+      autoclose_popup: true
     )
   end
   
+  subject { Buckaroo::Ideal::Config }
+  
   it 'has a merchant_key' do
-    Buckaroo::Ideal::Config.merchant_key.should == 'merchant_key'
+    subject.merchant_key.should == 'merchant_key'
   end
   
   it 'has a secret_key' do
-    Buckaroo::Ideal::Config.secret_key.should == 'secret_key'
+    subject.secret_key.should == 'secret_key'
   end
   
   it 'has a test_mode' do
-    Buckaroo::Ideal::Config.test_mode.should be_true
+    subject.test_mode.should be_true
+  end
+  
+  it 'has a success_url' do
+    subject.success_url.should == 'http://example.com/transaction/success'
+  end
+  
+  it 'has a reject_url' do
+    subject.reject_url.should == 'http://example.com/transaction/reject'
+  end
+  
+  it 'has a error_url' do
+    subject.error_url.should == 'http://example.com/transaction/error'
+  end
+  
+  it 'has a return_method' do
+    subject.return_method.should == 'GET'
+  end
+  
+  it 'has a style' do
+    subject.style.should == 'POPUP'
+  end
+  
+  it 'has a autoclose_popup setting' do
+    subject.autoclose_popup.should be_true
   end
   
   it 'can be reset to default values' do
-    Buckaroo::Ideal::Config.reset
-    Buckaroo::Ideal::Config.merchant_key.should be_nil
-    Buckaroo::Ideal::Config.secret_key.should   be_nil
-    Buckaroo::Ideal::Config.test_mode.should    be_false
+    subject.reset
+    subject.merchant_key.should    be_nil
+    subject.secret_key.should      be_nil
+    subject.test_mode.should       be_false
+    subject.success_url.should     be_nil
+    subject.reject_url.should      be_nil
+    subject.error_url.should       be_nil
+    subject.return_method.should   == 'POST'
+    subject.style.should           == 'PAGE'
+    subject.autoclose_popup.should be_false
   end
 end
