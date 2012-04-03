66aa1d0c687b829300d5c241269d3683ec0fb94b
Tom-Eric Gerritsen <ik@tom-eric.info>
2012-04-03 08:18:19 +0200

    Refactor Buckaroo::Ideal::Config

--- lib/buckaroo-ideal/config.rb
+++ lib/buckaroo-ideal/config.rb
@@ -2,22 +2,30 @@ module Buckaroo
   module Ideal
     class Config
       class << self
+        DEFAULTS = {
+          merchant_key: nil,
+          secret_key:   nil,
+          test_mode:    false
+        }
+        
         attr_accessor :merchant_key,
                       :secret_key,
                       :test_mode
         
         def configure(settings = {})
-          reset
-          
-          self.merchant_key = settings[:merchant_key]
-          self.secret_key   = settings[:secret_key]
-          self.test_mode    = settings[:test_mode]
+          DEFAULTS.merge(settings).each do |key, value|
+            set key, value
+          end
         end
         
         def reset
-          self.merchant_key = nil
-          self.secret_key   = nil
-          self.test_mode    = false
+          configure({})
+        end
+        
+        private
+        
+        def set(key, value)
+          instance_variable_set(:"@#{key}", value)
         end
       end
     end
