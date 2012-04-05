0b4bc214abfd6e754f5208ff4eca49e909d56d29
Tom-Eric Gerritsen <ik@tom-eric.info>
2012-04-05 07:19:27 +0200

    Reset Buckaroo::Ideal::Config on each spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,4 +12,8 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.mock_with :rspec
+  
+  config.before do
+    Buckaroo::Ideal::Config.reset
+  end
 end
